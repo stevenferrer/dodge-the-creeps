9604a5544a17e7850a6957cc43c4721762f93d4a
Steven Ferrer <steven.r.ferrer@gmail.com>
2022-05-02 16:06:52 +0800

    Added the HUD

--- Main.gd
+++ Main.gd
@@ -6,17 +6,24 @@ var score
 
 func _ready():
 	randomize()
-	new_game()
 
 
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
+	
+	get_tree().call_group("mobs", "queue_free")
+	
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready!")
 
 
 func _on_MobTimer_timeout():
@@ -40,6 +47,7 @@ func _on_MobTimer_timeout():
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_StartTimer_timeout():
